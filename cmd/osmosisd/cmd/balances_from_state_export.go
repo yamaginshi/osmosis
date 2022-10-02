65c8105221c438f05e30eaf25f282d2d6dfe6ef3
Adam Tucker <adam@osmosis.team>
2022-10-01 22:35:52 -0500

    test

--- cmd/osmosisd/cmd/balances_from_state_export.go
+++ cmd/osmosisd/cmd/balances_from_state_export.go
@@ -325,7 +325,7 @@ Example:
 
 			//snapshotOutput := args[1]
 
-			var derivedAccount []DerivedAccount
+			var derivedAccount DerivedAccount
 
 			// data in JSON format which
 			// is to be decoded
